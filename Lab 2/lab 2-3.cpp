8bb4c88c080245ca2e0ecf906aa17af63397a585
Савченко Алина <merwood@yandex.ru>
2017-10-17 01:06:06 +0500

    lab2

---
+++ Lab 2/lab 2-3.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>  
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter n = ";
+	cin >> n;
+	int *mas = new int[n];
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		cin >> mas[i];
+	}
+
+	int max, min;
+	max = mas[0];
+	min = mas[0];
+
+	for (i = 0; i < n; i++)
+	{
+		if (mas[i] < min)
+			min = mas[i];
+		if (mas[i] > max)
+			max = mas[i];
+	}
+
+	int k;
+	k = n;
+
+	for (i = 0; i < n; i++)
+	{
+		if (mas[i] == max)
+			k--;
+	}
+
+	int j = 0;
+
+	int *mas1 = new int[k];
+	for (j = 0; j < n; j++)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (mas[i] != max)
+				mas1[j] = mas[i];
+			else
+				mas1[j] = mas[i + 1];
+		}
+	}
+
+	for (j = 0; j < k; j++)
+	{
+		cout << mas1[j] << " ";
+	}
+
+	system("pause");
+	return 0;
+}
