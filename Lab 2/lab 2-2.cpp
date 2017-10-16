0676aab6116e1d8c7de29b79cea98224831a4e8a
Савченко Алина <merwood@yandex.ru>
2017-10-17 00:56:36 +0500

    lab 2

---
+++ Lab 2/lab 2-2.cpp
@@ -0,0 +1,40 @@
+﻿//создаем два массива. один для ввода переменных, второй для сравнения длин. находим максимальную и выводим.
+
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter n = ";
+	cin >> n;
+	int *mas = new int[n];
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		cin >> mas[i];
+	}
+
+	int k = 0, l = 0;
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] == a[i + 1])
+			k++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (i = 0; i < n-1; i++)
+		{
+			if (a[i] == a[i + 1])
+				k++;
+		}
+		if (k > l)
+		l = k;
+	}
+
+	cout << l;
+	system("pause");
+	return 0;
+}
