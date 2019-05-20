73b15a451810a5f4f1f8cdc2e70cc92030a6b16a
Savchenko Alina <31769352+branfield@users.noreply.github.com>
2019-05-20 14:32:47 +0500

    Update lab 6.cpp

--- labs/6/lab 6.cpp
+++ labs/6/lab 6.cpp
@@ -1,54 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 	setlocale(0, "");
-	int n=0;
+	int Z[100];
+	int n = 0;
+	int max = 0;
+	int n_max = 0;
+	int min = 0;
+	int n_min = 0;
+
 	cout << "Введите размерность массива: ";
 	cin >> n;
-	int *Z = new int[n];
-
 	for (int i = 0; i < n; i++)
-	{
 		cin >> Z[i];
-	}
-
-	int max = Z[0];
-	int min = Z[0];
-	int k = 0, m = 0;
+	max = Z[0];
+	min = Z[0];
 	for (int i = 0; i < n; i++)
 	{
-		if (Z[i] > max)
-		{
-			max = Z[i];
-			k = i;
-		}
-		if (Z[i] < min)
-		{
-			min = Z[i];
-			m = i;
-		}
+		if (Z[i] > max) { max = Z[i]; n_max = i; }
+		if (Z[i] < min) { min = Z[i]; n_min = i; }
 	}
 
-	for (int i = k; i < n-1; i++)
-	{
-			Z[i] = Z[i + 1]; 
-			n--;
-	}
-
-	for (int i = m; i < n-1; i++)
-	{
+	for (int i = n_max; i < n - 1; i++)
 		Z[i] = Z[i + 1];
-		n--;
-	}
-
+	n--;
+	for (int i = n_min; i < n - 1; i++)
+		Z[i] = Z[i + 1];
+	n--;
 	cout << "Полученный массив: ";
-	for (int i = 0; i < n; i++)
-	{
-		cout << Z[i] << " ";
-	}
-
+	for (int i = 0; i < n; i++) cout << Z[i] << " ";
 	cout << endl;
 	system("pause");
 	return 0;
